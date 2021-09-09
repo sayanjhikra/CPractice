f0c43fde05d5a2a91d0e4e1f175394e37bb7ce46
Sayan Mullick <39323283+sayanjhikra@users.noreply.github.com>
2021-09-09 09:12:53 +0530

    Add files via upload

---
+++ findevenodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+
+  int num = 26;
+  //scanf("%d", &num);
+  if(num%2 == 0)
+    printf("%d is an EVEN number", num);
+
+  else
+     printf("%d is an ODD number", num);
+
+}
