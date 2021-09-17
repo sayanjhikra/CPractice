e5eb7ead39defc05a1fc819634d6650d4ff19494
Sayan Mullick <39323283+sayanjhikra@users.noreply.github.com>
2021-09-17 19:29:06 +0530

    Add files via upload

---
+++ asciivalue.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+//find ascii value of charavter
+ char cha;
+void main(){
+    printf("enter only single character exm (A-Z,a-z,0-9,special char) :");
+ scanf("%c",&cha);
+printf("ASCII value of character '%c' is :%d",cha,cha);
+}
