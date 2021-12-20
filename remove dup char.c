aebf8ce22f47ddde4a1885bac6e7f4dc8b486c3a
Sayan Mullick <39323283+sayanjhikra@users.noreply.github.com>
2021-12-20 16:40:36 +0530

    Add files via upload

---
+++ remove dup char.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+int main (){
+    int i,s=0;
+    char stri[30];
+    printf("Enter string :");
+    gets(stri);
+   // scanf("%s",&stri);
+    while(stri[s]!='\0'){
+        s++;
+    }
+   // printf("%d",s);
+    for(i=0;i<s;i++){
+          if(stri[i]!='0'){
+            for(int k=i+1;k<s;k++){
+                if(stri[i]==stri[k]){
+                        int m;
+                    for(m=k;m<s;m++){
+                            char g;
+                        if(stri[m]=='0'){
+                            break;
+                        }
+                        stri[m]=stri[m+1];
+                    }
+                       stri[m]='0';
+                }
+            }
+          }
+    }
+
+    printf("%s",stri);
+}
