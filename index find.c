839b2e0efc555d0bd730dd49b630738b334016f3
Sayan Mullick <39323283+sayanjhikra@users.noreply.github.com>
2021-12-24 17:31:20 +0530

    Update index find.c

--- index find.c
+++ index find.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 
 
-int main ()
-{
+int main (){
     int i=0;
     char stri[100],srh[40];
     printf("Enter Sentance :");
@@ -10,39 +9,29 @@ int main ()
     printf("Enter word :");
     scanf("%s",&srh);
 
-    while(stri[i]!='\0')
-    {
-        if(stri[i]==' '||i==0)
-        {
+    while(stri[i]!='\0'){
+        if(stri[i]==' '||i==0){
             int s=1,j=0,p=0;
-            if(i!=0)
-            {
+            if(i!=0){
                 p=i+1;
             }
-
-            while(srh[s-1]!='\0')
-            {
-                if(srh[s-1]!=stri[(p+s)-1])
-                {
+            while(srh[s-1]!='\0'){
+                if(srh[s-1]!=stri[(p+s)-1]){
                     j=1;
                     break;
                 }
                 s++;
             }
-            if (stri[p+s-1]==' '||stri[p+s-1]=='\0')
-            {
+            if (stri[p+s-1]==' '||stri[p+s-1]=='\0'){
                 j=0;
             }
-            else
-            {
+            else{
                 j=1;
             }
-            if(j==0)
-            {
+            if(j==0){
                 printf("'%s' is found at index: %d\n",srh,p);
             }
         }
-
         i++;
     }
 }
