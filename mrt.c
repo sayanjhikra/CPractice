26758d08de2b19dcac36d56081e084522d2c417e
sayanjhikra <sayanjhikra@gmail.com>
2022-01-19 09:13:20 +0000

    update mrt.c

--- mrt.c
+++ mrt.c
@@ -2,27 +2,40 @@
 int shop(int,int),store(int,int);
 
 int main(){
-int inv=1000,mq=1000;
+int inv[4],mq=1000,i,c;
+//char nam();
 
+for(i=0;i<4;i++){
+inv[i]=1000;
+}
 while(1){
-if(inv>0){
-printf("%d items avialable\n",inv);
-inv=shop(inv,mq);
+printf("================SMART SHOP===============\n");
+printf("sope(1) shampo(2) toothpast(3) handwash(4)\n");
+printf("Enter what you want to buy :");
+scanf("%d",&c);
+if(c<5&&c>0){
+inv[c-1]=shop(inv[c-1],mq);
 }
 else{
-printf("Product Sold Out\n");
-break;
-
+printf("Enter option is not valid. plese enter right one\n");
 }
 }
 }
 
 int shop(int inv,int mq){
-int pq;
+int m,pq,i=1;
+char o;
+
+while(i==1){
+if(inv>0){
+printf("%d items avialable\n",inv);
 
 printf("Enter Purchasing Quantity :");
 scanf("%d",&pq);
+if(pq<=inv){
+i++;
 inv=inv-pq;
+printf("%d items purchased\n",pq);
 if(inv<=mq*.25){
 printf("place 75percent order in store\n");
 inv=inv+store(mq-inv,mq);
@@ -30,6 +43,35 @@ inv=inv+store(mq-inv,mq);
 if(inv<=mq*.50 && inv>mq*.25){
 printf("send alart to store\n");
 store(mq-inv,mq);
+}
+}
+else{
+printf("Entered quantity excid our availability\n");
+}
+}
+
+else{
+printf("Product Sold Out\n");
+}
+if(inv>0){
+m=1;
+printf("Do you want to buy more. press 'y' else press 'n':");
+while(m==1){
+scanf("\n%c",&o);
+if(o=='y'||o=='n'){
+if(o=='y'){
+i--;
+m++;
+}
+if(o=='n'){
+m++;
+}
+}
+else{
+printf("Only press 'y'-YES and 'n'-NO:");
+}
+}
+}
 }
 return inv;
 }
