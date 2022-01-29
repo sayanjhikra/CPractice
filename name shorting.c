681b01bbde3b5f4c6bf3dcffddcf8e52500c49c5
Sayan Mullick <39323283+sayanjhikra@users.noreply.github.com>
2022-01-29 22:44:13 +0530

    Update name shorting.c

--- name shorting.c
+++ name shorting.c
@@ -10,22 +10,41 @@ for(i=0;i<5;i++){
     gets(sarr[i]);
 }
 for(j=0;j<5;j++){
-    for(i=1;i<5;i++){
+    for(i=j+1;i<5;i++){
             m=0;
         while(sarr[j][m]!='\0'){
             if(sarr[j][m]==sarr[i][m]){
                 m++;
             }
             if(sarr[j][m]>sarr[i][m]){
-                char stri[10];
-                stri=sarr[j];
-                sarr[j]=sarr[i];
-                sarr[i]=stri;
+                char s[10];
+                int p=0;
+                while(sarr[j][p]!='\0'){
+                    s[p]=sarr[j][p];
+                    sarr[j][p]='\0';
+                    p++;
+                }
+                p=0;
+                while(sarr[i][p]!='\0'){
+                    sarr[j][p]=sarr[i][p];
+                    sarr[i][p]='\0';
+                    p++;
+                }
+                p=0;
+                while(s[p]!='\0'){
+                    sarr[i][p]=s[p];
+                    p++;
+                }
+                j=0;
+             break;
             }
             else{
                 break;
             }
         }
     }
+}
+for(i=0;i<5;i++){
+    printf("%d. %s\n",i+1,sarr[i]);
 }
 }
